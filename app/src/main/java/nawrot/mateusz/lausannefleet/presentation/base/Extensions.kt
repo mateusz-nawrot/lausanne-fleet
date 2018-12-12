b8438b740320188e347ff15cdbab75b94a344e82
Mateusz Nawrot <mnawrott@gmail.com>
2018-12-12 18:03:13 +0100

    improve car drawing mechanism; add toolbar menu entry

--- app/src/main/java/nawrot/mateusz/lausannefleet/presentation/base/Extensions.kt
+++ app/src/main/java/nawrot/mateusz/lausannefleet/presentation/base/Extensions.kt
@@ -1,9 +1,32 @@
 package nawrot.mateusz.lausannefleet.presentation.base
 
+import android.content.Context
+import com.google.android.gms.maps.model.BitmapDescriptor
 import com.google.android.gms.maps.model.LatLng
 import nawrot.mateusz.lausannefleet.domain.map.Position
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.VectorDrawable
+
+
 
 
 fun Position.toLatLng(): LatLng {
     return LatLng(lat, lng)
+}
+
+fun Context.getBitmapDescriptor(id: Int): BitmapDescriptor {
+    val vectorDrawable = getDrawable(id) as VectorDrawable
+
+    val width = vectorDrawable.intrinsicWidth
+    val height = vectorDrawable.intrinsicHeight
+
+    vectorDrawable.setBounds(0, 0, width, height)
+
+    val bm = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+    val canvas = Canvas(bm)
+    vectorDrawable.draw(canvas)
+
+    return BitmapDescriptorFactory.fromBitmap(bm)
 }
--- app/src/main/java/nawrot/mateusz/lausannefleet/presentation/map/MapActivity.kt
+++ app/src/main/java/nawrot/mateusz/lausannefleet/presentation/map/MapActivity.kt
@@ -1,30 +1,32 @@
 package nawrot.mateusz.lausannefleet.presentation.map
 
+import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
+import android.view.Menu
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
-import com.google.android.gms.common.ConnectionResult
-import com.google.android.gms.common.GoogleApiAvailability
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
-import com.google.android.gms.maps.model.MarkerOptions
-import com.google.android.gms.maps.model.PolylineOptions
-import com.google.maps.android.PolyUtil
+import com.google.android.gms.maps.model.*
 import dagger.android.AndroidInjection
 import nawrot.mateusz.lausannefleet.R
+import nawrot.mateusz.lausannefleet.domain.base.ErrorEvent
 import nawrot.mateusz.lausannefleet.domain.car.ActionType
+import nawrot.mateusz.lausannefleet.domain.car.Car
 import nawrot.mateusz.lausannefleet.domain.car.CarAction
+import nawrot.mateusz.lausannefleet.domain.station.Station
+import nawrot.mateusz.lausannefleet.presentation.base.getBitmapDescriptor
 import nawrot.mateusz.lausannefleet.presentation.base.toLatLng
 import javax.inject.Inject
 
-class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+const val POLYLINE_WIDTH = 6f
+
+class MapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener, GoogleMap.OnMapClickListener {
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
@@ -33,77 +35,141 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var map: GoogleMap
 
-    private val points = mutableListOf<LatLng>()
+    private val carMarkers = ArrayList<Marker>()
+    private val stationMarkers = ArrayList<Marker>()
+    private val routes = ArrayList<Polyline>()
 
-    private val markers = ArrayList<Marker>()
+    private val carIcon by lazy { getBitmapDescriptor(R.drawable.ic_car) }
+    private val blueMarkerIcon by lazy { BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE) }
+    private val redMarkerIcon by lazy { BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED) }
+
+    private var menuRef: Menu? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
+
         initViewModel()
         initMap()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.map_menu, menu)
+        menuRef = menu
+        return super.onCreateOptionsMenu(menu)
+    }
+
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
+        map.setOnMarkerClickListener(this)
+        map.setOnMapClickListener(this)
+        map.moveCamera(CameraUpdateFactory.newLatLng(LatLng(46.522873, 6.635265)))
+        map.animateCamera(CameraUpdateFactory.zoomTo(12f))
 
-        val station = LatLng(46.517202, 6.629205)
-        val station2 = LatLng(46.541050, 6.658185)
+        viewModel.getStations()
+    }
 
-        map.addMarker(MarkerOptions().position(station2).title("Station 2"))
+    override fun onMarkerClick(marker: Marker?): Boolean {
+        marker?.apply {
+            val markerTag = (tag as String)
+            if (markerTag.contains("station")) {
+                val id = markerTag.split(":")[1]
+                deselectAllMarkers()
+                marker.setIcon(redMarkerIcon)
 
-        map.moveCamera(CameraUpdateFactory.newLatLng(station))
-        map.animateCamera(CameraUpdateFactory.zoomTo(13f))
+                menuRef?.findItem(R.id.send_car)?.apply {
+                    isVisible = true
+                    setOnMenuItemClickListener {
+                        sendCarToStation(id)
+                    true
+                    }
+                }
+            }
+        }
+        return false
+    }
 
-        onStationClicked("1")
-//
-//        val line = "al|zGaxmg@VyDBCBGBSGQCAEo@@m@TgIRmFJwEBmBNiGNoDh@yCBUCg@E[MSsAmBa@g@}@i@iGuDOGk@pAa@rAkBnFMxAm@rA_@z@_AzAi@Yc@{@Ma@KyAOeAQk@_@[YI_@@O@WQQSS]@YHe@Va@t@w@NO@o@Ee@O[SQUE}FUU@_@Hw@b@c@V[HWAmCs@qA[qB[wDk@eASaA[cAc@i@]Y[g@o@yJyPMQo@[cAc@WG_@Co@@g@Fi@N_@@_CE?GGIGCGBEH[Ge@We@_@a@i@Uq@M}AKaACGMG_AEYS_EmAo@Uc@Yi@e@g@k@{AoBsAuA{DcDqC}B}AuA_CuBg@i@_AsAc@cAe@_Bm@qDo@sFiAaJa@iDdAYTIJM"
+    override fun onMapClick(point: LatLng?) {
+        menuRef?.findItem(R.id.send_car)?.isVisible = false
+        deselectAllMarkers()
+    }
 
+    private fun deselectAllMarkers(markerToExclude: String? = null) {
+        stationMarkers.forEach { it.setIcon(blueMarkerIcon) }
     }
 
     private fun initMap() {
-        if (isMapServiceAvailable()) {
-            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-            val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
-            mapFragment?.getMapAsync(this)
-        } else {
-            GoogleApiAvailability.getInstance().showErrorDialogFragment(this, GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this), 100)
-        }
-    }
-
-    private fun isMapServiceAvailable(): Boolean {
-        return GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this) == ConnectionResult.SUCCESS
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
+        mapFragment.getMapAsync(this)
     }
 
     private fun initViewModel() {
         viewModel = ViewModelProviders.of(this, viewModelFactory).get(MapViewModel::class.java)
+        viewModel.error().observe(this, Observer { error -> showError(error) })
+        viewModel.stations().observe(this, Observer { stations -> drawStations(stations) })
     }
 
-    private fun addPolyline(polyline: String, color: Int) {
-        val decoded = PolyUtil.decode(polyline)
-        points.addAll(decoded)
-        map.addPolyline(PolylineOptions().addAll(decoded).width(5f).color(color))
-    }
-
-    private fun onStationClicked(stationId: String) {
+    private fun sendCarToStation(stationId: String) {
         viewModel.addCar(stationId).observe(this, Observer { car -> car?.apply { handleCarAction(this) } })
     }
 
-    private fun handleCarAction(carAction: CarAction) {
-        Log.d("CAR_ACTION", carAction.toString())
-        when(carAction.actionType) {
+    private fun drawStations(stations: List<Station>) {
+        for (station in stations) {
+            val stationMarker = map.addMarker(MarkerOptions().position(station.position.toLatLng()).icon(blueMarkerIcon))
+            stationMarker.tag = "station:${station.id}"
+            stationMarkers.add(stationMarker)
+        }
+    }
+
+    private fun handleCarAction(action: CarAction) {
+        when (action.type) {
             ActionType.ADD -> {
-                val newMarker = map.addMarker(MarkerOptions().position(carAction.car.position.toLatLng()))
-                newMarker.tag = carAction.car.id.toString()
-                markers.add(newMarker)
+                addMarker(action.car)
+                addPolyline(action.car)
             }
             ActionType.MOVE -> {
-                markers.find { it.tag == carAction.car.id.toString()}?.position = carAction.car.position.toLatLng()
+                findCarMarker(action.id)?.position = action.car.position.toLatLng()
             }
             ActionType.REMOVE -> {
-                markers.find { it.tag == carAction.car.id.toString()}?.remove()
+                findCarMarker(action.id)?.remove()
+                findPolyline(action.id)?.remove()
             }
         }
     }
+
+    private fun showError(errorEvent: ErrorEvent) {
+        if (errorEvent.mapError) {
+            //TODO - show some dialog/textview?
+        }
+        //TODO - better error handling...
+        Toast.makeText(this, errorEvent.errorMessage, Toast.LENGTH_LONG).show()
+    }
+
+    private fun addMarker(car: Car) {
+        val newMarker = map.addMarker(MarkerOptions().position(car.position.toLatLng()).icon(carIcon).anchor(0.5f, 0.5f))
+        newMarker.tag = car.id
+        carMarkers.add(newMarker)
+    }
+
+    private fun addPolyline(car: Car) {
+        val route = car.route.map { it.toLatLng() }
+        val polyline = map.addPolyline(PolylineOptions().addAll(route).width(POLYLINE_WIDTH).color(getColor()))
+        polyline.tag = car.id
+        routes.add(polyline)
+    }
+
+    //TODO add random color
+    private fun getColor(): Int {
+        return Color.MAGENTA
+    }
+
+    private fun findCarMarker(id: String): Marker? {
+        return carMarkers.find { it.tag == id }
+    }
+
+    private fun findPolyline(id: String): Polyline? {
+        return routes.find { it.tag == id }
+    }
+
 }
