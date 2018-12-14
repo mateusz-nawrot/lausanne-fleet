9ef9ebf51af28c84eb90fc7a152877d8c94bafa5
Mateusz Nawrot <mnawrott@gmail.com>
2018-12-14 17:45:31 +0100

    add AddCarUseCaseTest.kt

---
+++ app/src/test/java/nawrot/mateusz/lausannefleet/AddCarUseCaseTest.kt
@@ -0,0 +1,56 @@
+package nawrot.mateusz.lausannefleet
+
+import io.reactivex.ObservableTransformer
+import io.reactivex.Single
+import nawrot.mateusz.lausannefleet.domain.base.SchedulersProvider
+import nawrot.mateusz.lausannefleet.domain.car.AddCarUseCase
+import nawrot.mateusz.lausannefleet.domain.car.CarEvent
+import nawrot.mateusz.lausannefleet.domain.car.CarRepository
+import nawrot.mateusz.lausannefleet.domain.map.Position
+import nawrot.mateusz.lausannefleet.domain.station.Station
+import nawrot.mateusz.lausannefleet.domain.station.StationRepository
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verify
+import org.mockito.MockitoAnnotations
+
+
+class AddCarUseCaseTest {
+
+    @Mock
+    private lateinit var schedulersProvider: SchedulersProvider
+
+    @Mock
+    private lateinit var stationRepository: StationRepository
+
+    @Mock
+    private lateinit var carRepository: CarRepository
+
+    private lateinit var useCase: AddCarUseCase
+
+    private val testOrigin = Station("1", Position(46.517202, 6.629205))
+    private val testDestination = Station("1", Position(46.517202, 6.629205))
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.initMocks(this)
+        useCase = AddCarUseCase(schedulersProvider, stationRepository, carRepository)
+
+        `when`(schedulersProvider.observableTransformer<CarEvent>()).thenReturn(ObservableTransformer { it })
+        `when`(stationRepository.getTripOrigin("1")).thenReturn(Single.just(testOrigin))
+        `when`(stationRepository.getTripDestination("1")).thenReturn(Single.just(testDestination))
+    }
+
+    @Test
+    fun `CarRepository gets correct trip origin and destination from StationRepository when AddCarUseCase is executed`() {
+        val testStationId = "1"
+
+        useCase.execute(testStationId).test()
+
+        verify(stationRepository).getTripOrigin(testStationId)
+        verify(stationRepository).getTripDestination(testStationId)
+        verify(carRepository).addCar(testOrigin.position, testDestination.position)
+    }
+}
